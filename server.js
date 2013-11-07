7f4c01089c5e87db6e22d3db4781350a150d5e58
Leonardo Meirelles <pitecus@gmail.com>
2013-11-06 22:28:19 -0500

    Removed some blank lines.

--- server.js
+++ server.js
@@ -2,11 +2,17 @@
 (function () {
   "use strict";
   var
+    // Express framework.
     express = require('express'),
+    // Authentication.
     passport = require('passport'),
     GoogleStrategy = require('passport-google').Strategy,
     LocalStrategy = require('passport-local').Strategy,
+    //
+    user = require('./schemas/UserSchema'),
+    // Utility method to verify authenticaton.
     ensureAuthenticated,
+    // The express application.
     app;
 
   // Ensure user is authenticated.
@@ -20,13 +26,7 @@
     res.redirect('/logout');
   };
 
-  // Passport session setup.
-  //   To support persistent login sessions, Passport needs to be able to
-  //   serialize users into and deserialize users out of the session. Typically,
-  //   this will be as simple as storing the user ID when serializing, and finding
-  //   the user by ID when deserializing. However, since this example does not
-  //   have a database of user records, the complete Google profile is serialized
-  //   and deserialized.
+  // To support persistent login sessions, Passport needs to be able to serialize users into and deserialize users out of the session. Typically, this will be as simple as storing the user ID when serializing, and finding the user by ID when deserializing. However, since this example does not have a database of user records, the complete Google profile is serialized and deserialized.
   passport.serializeUser(function (user, done) {
     done(null, user);
   });
@@ -42,7 +42,6 @@
     profile.identifier = identifier;
     return done(null, profile);
   }));
-
   // Use the Local Strategy within Passport.
   passport.use(new LocalStrategy(function (username, password, done) {
     console.log('username=[%s], password=[%s]', username, password);
@@ -61,7 +60,6 @@
 
   // Create server.
   app = express.createServer();
-
   // Configure Express
   app.configure(function () {
     // Setup the Template Engine.
@@ -97,22 +95,18 @@
   app.get('/', function (req, res) {
     res.render('index', { user: req.user });
   });
-
   // Show contact information.
   app.get('/contact', function (req, res) {
     res.render('contact', { user: req.user });
   });
-
   // Use passport.authenticate() as route middleware to authenticate the request. The first step in Google authentication will involve redirecting the user to google.com. After authenticating, Google will redirect the user back to this application at /auth/google/return
   app.get('/auth/google', passport.authenticate('google', {failureRedirect: '/logout'}), function (req, res) {
     res.redirect('/');
   });
-
   // Use passport.authenticate() as route middleware to authenticate the request. If authentication fails, the user will be redirected back to the login page. Otherwise, the primary route function function will be called, which, in this example, will redirect the user to the home page.
   app.get('/auth/google/return', passport.authenticate('google', {'successRedirect': '/account', 'failureRedirect': '/logout'}), function (req, res) {
     res.redirect('/');
   });
-
   // Perform the authentication against local credentials.
   app.post('/auth/local', passport.authenticate('local', {successRedirect: '/account', failureRedirect: '/logout'}), function (req, res) {
     var
@@ -124,12 +118,10 @@
     pass = req.param('password');
     console.log('app.post, email=[%s], pass=[%s]', email, pass);
   });
-
   // Show the information regarding the account. It should only show when authenticated.
   app.get('/account', ensureAuthenticated, function (req, res) {
     res.render('account', { user: req.user });
   });
-
   // Perform the logout and redirect to home.
   app.get('/logout', function (req, res) {
     req.logout();
